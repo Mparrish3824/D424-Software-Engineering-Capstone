acdb347d81e8841a9bec16de7cccdd7801e953b0
Mparrish3824 <125155237+Mparrish3824@users.noreply.github.com>
2025-09-25 17:01:46 -0700

    Add files via upload

---
+++ src/main/java/com/d424capstone/demo/controllers/HealthController.java
@@ -0,0 +1,20 @@
+package com.d424capstone.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping ("/health")
+    public Map<String, String> health() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("timestamp", LocalDateTime.now().toString());
+        return response;
+    }
+}
