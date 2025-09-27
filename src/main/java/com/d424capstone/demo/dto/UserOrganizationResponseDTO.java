38178cce37bc0df6248a4e77af00d1b4b87bf20b
Mparrish3824 <125155237+Mparrish3824@users.noreply.github.com>
2025-09-27 12:51:56 -0700

    Add files via upload

---
+++ src/main/java/com/d424capstone/demo/dto/UserOrganizationResponseDTO.java
@@ -0,0 +1,36 @@
+package com.d424capstone.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserOrganizationResponseDTO {
+    private Integer id;
+    private Integer userId;
+    private String username;
+    private String userEmail;
+    private String firstName;
+    private String lastName;
+    private Integer orgId;
+    private String orgName;
+    private String orgCode;
+    private String orgRole;
+    private String joinedAt;
+
+    public UserOrganizationResponseDTO() {}
+
+    public UserOrganizationResponseDTO(Integer id, Integer userId, String username, String userEmail, String firstName, String lastName, Integer orgId, String orgName, String orgCode, String userRole, String joinedDate) {
+        this.id = id;
+        this.userId = userId;
+        this.username = username;
+        this.userEmail = userEmail;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.orgId = orgId;
+        this.orgName = orgName;
+        this.orgCode = orgCode;
+        this.orgRole = userRole;
+        this.joinedAt = joinedDate;
+    }
+}
