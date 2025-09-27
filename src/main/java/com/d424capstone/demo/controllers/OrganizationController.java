a1918648695b5878b40df0edd21e8e53c7b3bda3
Mparrish3824 <125155237+Mparrish3824@users.noreply.github.com>
2025-09-26 18:13:08 -0700

    Update OrganizationController.java

--- src/main/java/com/d424capstone/demo/controllers/OrganizationController.java
+++ src/main/java/com/d424capstone/demo/controllers/OrganizationController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.d424capstone.demo.dto.CreateOrgRequest;
 
 import java.util.List;
 
@@ -98,6 +99,7 @@ public class OrganizationController {
 
     
 }
+
 
 
 
