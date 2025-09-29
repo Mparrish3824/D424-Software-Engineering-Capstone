c4ad604465d0dcc593d509c3850942d64c09248a
Mparrish3824 <125155237+Mparrish3824@users.noreply.github.com>
2025-09-29 15:00:06 -0700

    Update CorsConfig.java

--- src/main/java/com/d424capstone/demo/config/CorsConfig.java
+++ src/main/java/com/d424capstone/demo/config/CorsConfig.java
@@ -14,10 +14,15 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200", "https://update-firebase-app.web.app")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://nonprofit-event-management-app.web.app",
+                        "https://nonprofit-event-management-app.firebaseapp.com"
+                )                
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
+
 
